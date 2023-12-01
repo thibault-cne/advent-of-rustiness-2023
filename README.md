0e0f5c9a14859f725d770775b1eb7902b2269ee6
Thibault Cheneviere <thibault.chene23@gmail.com>
2023-12-01 18:09:49 -0500

    [~] updated readme and reworked the part two solution to have a more elegant solving

--- README.md
+++ README.md
@@ -18,11 +18,11 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 ## Benchmarks
 
-|           Day            |      Part 1       |        Part 2        |
-| :----------------------: | :---------------: | :------------------: |
-| [Day 1](./src/bin/01.rs) | `34.1µs` `16 KiB` | `44.7µs` / `16.6 KB` |
+|           Day            |       Part 1        |        Part 2        |
+| :----------------------: | :-----------------: | :------------------: |
+| [Day 1](./src/bin/01.rs) | `28.3µs` / `16 KiB` | `146.4µs` / `16 KiB` |
 
-**Total time: 0.08ms**
+**Total time: 0.17ms**
 
 <!--- benchmarking table --->
 
@@ -88,8 +88,8 @@ cargo solve <day>
 # output:
 #     Finished dev [unoptimized + debuginfo] target(s) in 0.13s
 #     Running `target/debug/01`
-# Part 1: 42 (166.0ns)
-# Part 2: 42 (41.0ns)
+# Part 1: 42 (166.0ns) (10 KiB)
+# Part 2: 42 (41.0ns) (10 KiB)
 ```
 
 The `solve` command runs your solution against real puzzle inputs. To run an optimized build of your code, append the `--release` flag as with any other rust program.
@@ -115,8 +115,8 @@ cargo all
 # ----------
 # | Day 01 |
 # ----------
-# Part 1: 42 (19.0ns)
-# Part 2: 42 (19.0ns)
+# Part 1: 42 (19.0ns) (10 KiB)
+# Part 2: 42 (19.0ns) (10 KiB)
 # <...other days...>
 # Total: 0.20ms
 ```
@@ -200,13 +200,6 @@ Go to the _Variables_ tab in your repository settings and create the following v
 ### Check code formatting / clippy lints in CI
 
 Uncomment the respective sections in the `ci.yml` workflow.
-
-### Use VS Code to debug your code
-
-1. Install [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer) and [CodeLLDB](https://marketplace.visualstudio.com/items?itemName=vadimcn.vscode-lldb).
-2. Set breakpoints in your code. [^3]
-3. Click _Debug_ next to the unit test or the _main_ function. [^4]
-4. The debugger will halt your program at the specific line and allow you to inspect the local stack. [^5]
 
 ## Useful crates
 
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -17,56 +17,68 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let lines = input.split('\n').collect::<Vec<&str>>();
     let mut ans = 0;
-    let numbers = vec![
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ];
 
     for l in lines.iter().take(lines.len() - 1) {
-        let mut first_digit = None;
-        let mut last_digit = None;
-
-        for (i, c) in l.char_indices() {
-            if first_digit.is_none() {
-                first_digit = if c.is_ascii_digit() {
-                    Some(c.to_digit(10).unwrap())
-                } else {
-                    numbers
-                        .iter()
-                        .find(|(s, _)| l[i..].starts_with(s))
-                        .map(|(_, n)| *n)
-                };
-            }
-
-            if last_digit.is_none() {
-                let index = l.len() - i - 1;
-                let c = l.as_bytes()[index] as char;
-                last_digit = if c.is_ascii_digit() {
-                    Some(c.to_digit(10).unwrap())
-                } else {
-                    numbers
-                        .iter()
-                        .find(|(s, _)| l[..=index].ends_with(s))
-                        .map(|(_, n)| *n)
-                };
-            }
-
-            if first_digit.is_some() && last_digit.is_some() {
-                break;
-            }
-        }
-
-        ans += first_digit.unwrap() * 10 + last_digit.unwrap();
+        ans += Parser::parse(l);
     }
 
     Some(ans)
+}
+
+struct Parser;
+
+impl Parser {
+    fn parse(input: &str) -> u32 {
+        let mut iter = input.chars().peekable();
+        let mut first = u32::MAX;
+        let mut last = u32::MAX;
+
+        while let Some(c) = iter.next() {
+            // If c is a digit
+            let digit = if c.is_ascii_digit() {
+                c.to_digit(10)
+            } else {
+                match c {
+                    'o' if try_parse(&mut iter, "one") => Some(1),
+                    't' if try_parse(&mut iter, "two") => Some(2),
+                    't' if try_parse(&mut iter, "three") => Some(3),
+                    'f' if try_parse(&mut iter, "four") => Some(4),
+                    'f' if try_parse(&mut iter, "five") => Some(5),
+                    's' if try_parse(&mut iter, "six") => Some(6),
+                    's' if try_parse(&mut iter, "seven") => Some(7),
+                    'e' if try_parse(&mut iter, "eight") => Some(8),
+                    'n' if try_parse(&mut iter, "nine") => Some(9),
+                    _ => None,
+                }
+            };
+
+            if let Some(digit) = digit {
+                if first == u32::MAX {
+                    first = digit;
+                }
+
+                last = digit;
+            }
+        }
+
+        first * 10 + last
+    }
+}
+
+fn try_parse<I: Iterator<Item = char>>(iter: &mut std::iter::Peekable<I>, pattern: &str) -> bool {
+    let mut index = 1;
+    let chars = pattern.as_bytes();
+
+    while let Some(peek) = iter.peek() {
+        if index == pattern.len() || *peek != chars[index] as char {
+            break;
+        }
+
+        iter.next();
+        index += 1;
+    }
+
+    index == pattern.len()
 }
 
 #[cfg(not(feature = "test_lib"))]
@@ -85,5 +97,43 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_example(DAY, PART_TWO));
         assert_eq!(result, Some(281));
+    }
+
+    macro_rules! test_parser {
+        ($(($lit:literal, $expected:expr)),*) => {
+            $(
+                assert_eq!(Parser::parse($lit), $expected);
+            )*
+        };
+    }
+
+    #[test]
+    fn test_parser() {
+        test_parser!(
+            ("onetwo3aanvabvjadbv9", 19),
+            ("abvhvatwo1", 21),
+            ("1", 11),
+            ("1onetwo", 12)
+        );
+    }
+
+    macro_rules! try_parse {
+        ($(($lit:literal, $prefix:literal, $expected:expr, $end:literal)),*) => {
+            $(
+                let mut iter = $lit.chars().peekable();
+                iter.next();
+                assert_eq!(try_parse(&mut iter, $prefix), $expected);
+                assert_eq!(&iter.collect::<String>(), $end);
+            )*
+        };
+    }
+
+    #[test]
+    fn test_try_parse() {
+        try_parse!(
+            ("onetwo1", "one", true, "two1"),
+            ("otwo1two", "one", false, "two1two"),
+            ("three", "three", true, "")
+        );
     }
 }
