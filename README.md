293894737e6b08bc2a23a70f83ce95abd46a3164
Thibault Cheneviere <thibault.chene23@gmail.com>
2023-12-08 03:01:48 -0500

    [+] add day 8

--- README.md
+++ README.md
@@ -23,15 +23,16 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `36.7µs` / `16 KiB` | `79.8µs` / `16 KiB` |
-| [Day 2](./src/bin/02.rs) | `43.1µs` / `2432 B` | `43.2µs` / `2432 B` |
-| [Day 3](./src/bin/03.rs) | `84.8µs` / `4112 B` | `49.0µs` / `4112 B` |
-| [Day 4](./src/bin/04.rs) | `210.7µs` / `43844 B` | `200.8µs` / `43844 B` |
-| [Day 5](./src/bin/05.rs) | `20.9µs` / `8736 B` | `1.4s` / `8736 B` |
-| [Day 6](./src/bin/06.rs) | `235.0ns` / `32 B` | `14.2ms` / `143 B` |
-| [Day 7](./src/bin/07.rs) | `390.6µs` / `24712 B` | `386.0µs` / `24712 B` |
+| [Day 1](./src/bin/01.rs) | `36.7µs` / `16 KiB` | `76.7µs` / `16 KiB` |
+| [Day 2](./src/bin/02.rs) | `43.1µs` / `2432 B` | `43.7µs` / `2432 B` |
+| [Day 3](./src/bin/03.rs) | `84.5µs` / `4112 B` | `48.9µs` / `4112 B` |
+| [Day 4](./src/bin/04.rs) | `213.5µs` / `43844 B` | `202.5µs` / `43844 B` |
+| [Day 5](./src/bin/05.rs) | `21.0µs` / `8736 B` | `1.4s` / `8736 B` |
+| [Day 6](./src/bin/06.rs) | `209.0ns` / `32 B` | `14.2ms` / `143 B` |
+| [Day 7](./src/bin/07.rs) | `375.4µs` / `24712 B` | `377.4µs` / `24712 B` |
+| [Day 8](./src/bin/08.rs) | `612.4µs` / `124728 B` | `6.3ms` / `124728 B` |
 
-**Total time: 1415.75ms**
+**Total time: 1422.64ms**
 
 <!--- benchmarking table --->
 
---
+++ src/bin/08.rs
@@ -0,0 +1,159 @@
+advent_of_code::solution!(8);
+
+use std::collections::HashMap;
+
+use nom::{
+    bytes::complete::{is_a, tag},
+    character::complete::{self, alphanumeric1, line_ending},
+    multi::many1,
+    sequence::{delimited, separated_pair, terminated},
+    IResult,
+};
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (rem, mut directions) = parse_directions(input).expect("parsing error");
+    let (_, map) = parse_map(rem).expect("parsing error");
+    let mut steps = 0;
+    let mut current = "AAA";
+
+    while current != "ZZZ" {
+        if directions[0] == Direction::Left {
+            current = map[&current].0;
+        } else {
+            current = map[&current].1;
+        };
+
+        directions.rotate_left(1);
+        steps += 1;
+    }
+
+    Some(steps)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let (rem, directions) = parse_directions(input).expect("parsing error");
+    let (_, map) = parse_map(rem).expect("parsing error");
+    let mut cycles = Vec::new();
+
+    map.keys().filter(|k| k.ends_with('A')).for_each(|current| {
+        let mut current = current;
+        let mut cycle = Vec::new();
+        let mut index = 0;
+        let mut steps = 0;
+        let mut first_z = None;
+
+        loop {
+            while steps == 0 || !current.ends_with("Z") {
+                steps += 1;
+                match directions[index] {
+                    Direction::Left => current = &map[current].0,
+                    _ => current = &map[current].1,
+                }
+                index = (index + 1) % directions.len();
+            }
+
+            cycle.push(steps);
+
+            if first_z.is_none() {
+                first_z = Some(current);
+                steps = 0;
+            } else if current == first_z.unwrap() {
+                break;
+            }
+        }
+
+        cycles.push(cycle[0]);
+    });
+
+    Some(lcm_vec(&cycles) as u64)
+}
+
+fn lcm_vec(vec: &[usize]) -> usize {
+    let mut result = vec[0];
+    for i in 1..vec.len() {
+        result = lcm(result, vec[i]);
+    }
+    return result;
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    return a * (b / gcd(a, b));
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    if a == b {
+        return a;
+    }
+    if b > a {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    while b > 0 {
+        let temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
+}
+
+fn parse_directions(input: &str) -> IResult<&str, Vec<Direction>> {
+    let (rem, directions) = terminated(is_a("LR"), many1(complete::newline))(input)?;
+
+    Ok((
+        rem,
+        directions.chars().map(|c| Direction::from(c)).collect(),
+    ))
+}
+
+fn parse_map(input: &str) -> IResult<&str, HashMap<&str, (&str, &str)>> {
+    let pair = delimited(
+        complete::char('('),
+        separated_pair(alphanumeric1, tag(", "), alphanumeric1),
+        complete::char(')'),
+    );
+    let line = terminated(separated_pair(alphanumeric1, tag(" = "), pair), line_ending);
+    let (rem, items) = many1(line)(input)?;
+
+    let mut map = HashMap::new();
+
+    items.iter().for_each(|&(from, (left, right))| {
+        map.insert(from, (left, right));
+    });
+
+    Ok((rem, map))
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+enum Direction {
+    Right,
+    Left,
+}
+
+impl From<char> for Direction {
+    fn from(value: char) -> Self {
+        match value {
+            'L' => Self::Left,
+            'R' => Self::Right,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    advent_of_code::part!();
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_example(DAY, PART_ONE));
+        assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_example(DAY, PART_TWO));
+        assert_eq!(result, Some(6));
+    }
+}
